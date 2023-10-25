8b3a683f991dabc2818e10c0519d617135d1ec10
bharatchowdarykolla <bharatkolla11@gmail.com>
2023-10-25 19:56:08 -0400

    Initial Commit

---
+++ MyLoginPageSwiftUI/MyLoginPageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLoginPageSwiftUIApp.swift
+//  MyLoginPageSwiftUI
+//
+//  Created by Bharat chowdary Kolla on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MyLoginPageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
